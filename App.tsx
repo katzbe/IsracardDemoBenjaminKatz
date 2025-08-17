fd0918b2a64280733e372dec39b5a7a3843fccff
Benjamin Katz <benjaminkatz@Benjamins-MacBook-Pro.local>
2025-08-17 21:13:31 +0300

    splash

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
 import { DefaultTheme } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BootSplash from 'react-native-bootsplash';
 
 import Navigation from './src/navigation/RootNavigator';
+import { delay } from './src/utils';
 
 const MyTheme = {
   ...DefaultTheme,
@@ -15,6 +18,20 @@ const MyTheme = {
 const queryClient = new QueryClient();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      // Simulate some startup logic before hiding the splash screen
+      await delay(1500);
+      await BootSplash.hide({ fade: true });
+      setIsLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) return null;
+
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView>
